532e558d88dc667d639a80f77c3a37f0868bf98c
Nathan <ncl9100@nyu.edu>
2025-08-10 00:26:26 -0700

    fixed broken api issues, version 1.0.1 on app store

--- app.config.js
+++ app.config.js
@@ -1,41 +1,44 @@
 import 'dotenv/config';
 
 export default {
-  name: "TechCompanionApp",
-  slug: "TechCompanionApp",
-  version: "1.0.0",
+  name: "ElderlyAI",
+  slug: "elderlyai",
+  owner: "gamerguy123",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
   newArchEnabled: true,
   splash: {
-    image: "./assets/splash-icon.png",
+    image: "./assets/icon.png",
     resizeMode: "contain",
     backgroundColor: "#ffffff"
   },
   ios: {
     supportsTablet: true,
     bundleIdentifier: "com.anonymous.TechCompanionApp",
+    buildNumber: "2",
     infoPlist: {
       ITSAppUsesNonExemptEncryption: false,
       NSPhotoLibraryUsageDescription: "This app may access your photo library to help you learn how to use your iPhone's camera and photos features. This helps provide better guidance when teaching you about your device.",
       NSSpeechRecognitionUsageDescription: "This app uses speech recognition to help you interact with the AI assistant more easily. You can speak your questions instead of typing them."
     }
+    
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: "./assets/adaptive-icon.png",
+      foregroundImage: "./assets/icon.png",
       backgroundColor: "#ffffff"
     },
     edgeToEdgeEnabled: true,
     package: "com.anonymous.TechCompanionApp"
   },
   web: {
-    favicon: "./assets/favicon.png"
+    favicon: "./assets/icon.png"
   },
   extra: {
     eas: {
-      projectId: "ce609487-c5a0-426f-892c-e1198853aa5e"
+      projectId: "2c04889e-6930-49e7-9e26-179472fc3f85"
     },
     newsApiKey: process.env.NEWS_API_KEY,
     openaiApiKey: process.env.OPENAI_API_KEY,
